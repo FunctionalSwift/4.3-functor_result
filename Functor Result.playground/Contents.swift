6746816b70301dab90e9975e268d5e5acc884f0e
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-11 00:49:31 +0100

    The result of Result + Map is: Functor Result

--- Functor Result.playground/Contents.swift
+++ Functor Result.playground/Contents.swift
@@ -1,1 +1,16 @@
 //: Playground - Functor Result
+
+enum Result<A, E> {
+    case success(A)
+    case failure(E)
+    
+    func map<B>(_ transform: (A) -> B) -> Result<B, E> {
+        switch self {
+        case let .success(value):
+            return .success(transform(value))
+        case let .failure(reason):
+            return .failure(reason)
+        }
+    }
+}
+
