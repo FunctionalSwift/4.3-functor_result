faf69de215534b71097037f0f88511e48da59053
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-11 01:21:46 +0100

    Functor Result is awesome!!

--- Functor Result.playground/Contents.swift
+++ Functor Result.playground/Contents.swift
@@ -22,8 +22,17 @@ func mustBeInt(_ string: String) -> Result<Int, Error> {
     return Int(string).map { .success($0) } ?? .failure(.mustBeInt)
 }
 
-mustBeInt("10") // success(10)
+func add2(_ number: Int) -> Int {
+    return number + 2
+}
 
-mustBeInt("5") // success(5)
+func double(_ number: Int) -> Int {
+    return number * 2
+}
 
-mustBeInt("swift") // failure(Error.mustBeInt)
+mustBeInt("10").map(add2) // Success(12)
+
+mustBeInt("5").map(double) // Success(10)
+
+mustBeInt("swift").map(add2) // Failure(Error.MustBeInt)
+
--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 ## Video 3: Functor Result
 
 Copyright © [FunctionalHub.com](http://functionalHub.com) 2018. All rights reserved.
+
+<p align="center">
+<img src="./images/code.png" alt="Code" width="600"/>
+</p>
